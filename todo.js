babad492be013491c67af264c61c4f849ef7e6df
dhruvkoka <dhrumuk@gmail.com>
2025-08-15 13:23:48 -0500

    update

--- todo.js
+++ todo.js
@@ -2,22 +2,45 @@ const todo=document.getElementById("todo")
 let insert=document.getElementById("insert")
 let del=document.getElementById("delete")
 let output=document.getElementById("output")
-let value=todo.value
+let todosList=[]
+let checkforPrevious=localStorage.getItem("todosList")
+
+function checkPrevious(){
+    if(checkforPrevious){
+        todosList=checkforPrevious
+        render()
+    }
+}
+
 
 
 insert.addEventListener("click",function(){
-    let value=todo.value
-    localStorage.setItem("value",JSON.stringify(value))
-    output.textContent=JSON.parse(localStorage.getItem("value"))
+    if (todo.value===""){
+        void(0)
+    }
+    else{
+        todosList.push(todo.value)
+        localStorage.setItem("todosList",todosList)
+        todo.value=""
+        render()
+    }
 
+       
 
 })
+
 del.addEventListener("click",function(){
     localStorage.clear()
-    output.textContent=""
-    todo.value=""
+    todosList=[]
+    output.innerHTML=""
+    
+
 
 })
-
-
-
+function render(){
+    finallist=""
+    for (let i=0;i<todosList.length;i++){
+        finallist+=`<li>${todosList[i]}</li>`
+    }
+    output.innerHTML=finallist
+}
